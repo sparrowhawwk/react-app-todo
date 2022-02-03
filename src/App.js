f706018f531942436d5f913ac4cccd8af60f747c
Chandrasekar Arunachalam <chs@ymail.com>
2022-02-03 09:43:58 +0530

    Removed unwanted files and wired by events.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    function addTask(name) {
+        alert(name);
+    }
+    const taskList = props.tasks?.map((task) => (
+        <Todo
+            id={task.id}
+            name={task.name}
+            completed={task.completed}
+            key={task.id}
+        />
+    ));
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form addTask={addTask} />
+            <div className="filters btn-group stack-exception">
+                <FilterButton />
+                <FilterButton />
+                <FilterButton />
+            </div>
+            <h2 id="list-heading">3 tasks remaining</h2>
+            <ul
+                role="list"
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading"
+            >
+                {taskList}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
