6d223b4f5ac4bab4399677edc767f02108c01606
Chandrasekar Arunachalam <chs@ymail.com>
2022-02-04 20:50:47 +0530

    Added feature to add a new task.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
 function App(props) {
-    function addTask(name) {
-        alert(name);
-    }
-    const taskList = props.tasks?.map((task) => (
+    const [tasks, setTasks] = useState(props.tasks);
+
+    const taskList = tasks.map((task) => (
         <Todo
             id={task.id}
             name={task.name}
             completed={task.completed}
             key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
         />
     ));
+
+    const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+    function addTask(name) {
+        const newTask = {
+            id: 'todo-' + nanoid(),
+            name: name,
+            completed: false,
+        };
+        setTasks([...tasks, newTask]);
+    }
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            if (id === task.id) {
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
@@ -23,7 +48,7 @@ function App(props) {
                 <FilterButton />
                 <FilterButton />
             </div>
-            <h2 id="list-heading">3 tasks remaining</h2>
+            <h2 id="list-heading">{headingText}</h2>
             <ul
                 role="list"
                 className="todo-list stack-large stack-exception"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
 function Form(props) {
+    const [name, setName] = useState('');
     function handleSubmit(e) {
         e.preventDefault();
-        props.addTask('Say Hello');
+        props.addTask(name);
+        setName('');
     }
-    const [name, setName] = useState('Use hooks');
+    function handleChange(e) {
+        setName(e.target.value);
+    }
     return (
         <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
@@ -19,6 +23,8 @@ function Form(props) {
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
